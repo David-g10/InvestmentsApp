c87011c77f4ae9ab0809745499fbe090f6cad5bb
david-g10 <giraldovdavid@gmail.com>
2023-10-09 11:12:15 -0500

    vote feature modified

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -12,6 +12,11 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.VoteBase, db: Session = Depends(orm_database.get_db),current_user: int = Depends(oauth2.get_current_user)):
+    
+    post = db.query(orm_models.Post).filter(orm_models.Post.id == vote.post.id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post with id: {vote.post.id} not found")
+
     vote_query = db.query(orm_models.Vote).filter(orm_models.Vote.investment_id == vote.investment_id, orm_models.Vote.user_id == current_user["id"])
     found_vote = vote_query.first()
     if vote.dir == 1:
